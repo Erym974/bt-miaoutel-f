d8ca2d32cbab838e03858e6daaf7b2ffcc71656b
Rémy <remyrichard974@live.fr>
2025-02-01 22:11:46 +0100

    Update config.tsx

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,5 @@
 export default {
     SOCKET_BASE_URL: 'https://bt-miaoutel-b.onrender.com',
     BACK_URL: 'https://bt-miaoutel-b.onrender.com',
-    FRONT_URL: 'https://bt-miaoutel-f.onrender.com'
+    FRONT_URL: 'https://splendorous-sawine-a01f44.netlify.app'
 }
